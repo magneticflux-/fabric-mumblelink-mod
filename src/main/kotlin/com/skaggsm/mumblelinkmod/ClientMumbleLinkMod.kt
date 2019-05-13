14dca49fa49680a47fb5fd9b44e9ec763a523110
Mitchell Skaggs <skaggsm333@gmail.com>
2019-05-13 13:13:45 -0500

    Add data to MumbleLink object

--- src/main/kotlin/com/skaggsm/mumblelinkmod/ClientMumbleLinkMod.kt
+++ src/main/kotlin/com/skaggsm/mumblelinkmod/ClientMumbleLinkMod.kt
@@ -6,32 +6,68 @@ import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.event.client.ClientTickCallback
 import net.minecraft.util.math.Vec3d
 
+private val Vec3d.toArray: FloatArray
+    get() = floatArrayOf(x.toFloat(), y.toFloat(), z.toFloat())
+
 /**
  * Created by Mitchell Skaggs on 5/12/2019.
  */
 object ClientMumbleLinkMod : ClientModInitializer {
-    lateinit var mumble: MumbleLink
+    private var mumble: MumbleLink? = null
 
     override fun onInitializeClient() {
-        println("Initializing MumbleLink Client")
-        mumble = MumbleLinkImpl()
-
         ClientTickCallback.EVENT.register(ClientTickCallback {
-            if (it.world != null /*&& !it.isInSingleplayer*/) {
-                mumble.name = "Minecraft MumbleLink Mod"
+            if (it.world != null) {
+                val mumble = ensureLinked()
+
+                val camPos = it.player.getCameraPosVec(1F)
+                val camDir = it.player.rotationVecClient
+
                 mumble.uiVersion = 2
                 mumble.uiTick++
 
-                // Location
-                val location = it.player.pos.add(
-                        Vec3d(0.0,
-                                it.player.getEyeHeight(it.player.pose).toDouble(),
-                                0.0))
+                mumble.avatarPosition = camPos.toArray
+                mumble.avatarFront = camDir.toArray
+                //mumble.avatarTop
 
+                mumble.name = "Minecraft MumbleLink Mod"
 
-                // Looking direction
-                val direction = it.player.getCameraPosVec(1F)
+                mumble.cameraPosition = camPos.toArray
+                mumble.cameraFront = camDir.toArray
+                //mumble.cameraTop
+
+                mumble.identity = it.player.uuidAsString
+
+                mumble.context = "${it.world.dimension.type}-${it.player.scoreboardTeam?.name}"
+                println("${mumble.identity}@${mumble.context}")
+
+                mumble.description = "A Minecraft mod that provides position data to Mumble."
+            } else {
+                ensureClosed()
             }
         })
+    }
+
+    private fun ensureLinked(): MumbleLink {
+        var localMumble = mumble
+
+        if (localMumble != null)
+            return localMumble
+
+        println("Linking to Mumble...")
+        localMumble = MumbleLinkImpl()
+        mumble = localMumble
+        println("Linked")
+
+        return localMumble
+    }
+
+    private fun ensureClosed() {
+        if (mumble != null) {
+            println("Unlinking from Mumble...")
+            mumble?.close()
+            mumble = null
+            println("Unlinked")
+        }
     }
 }
